3248012c3afca0e4e83298981c880583f84cf5bf
Huda Fathur <huda.fathurr@gmail.com>
2021-06-17 16:01:00 +0700

    Update multi_label_classification.py

--- modules/multi_label_classification.py
+++ modules/multi_label_classification.py
@@ -6,7 +6,7 @@ import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 
-from transformers.activations import gelu, gelu_new
+from transformers.activations import gelu, gelu_new, swish
 from transformers.configuration_bert import BertConfig
 from transformers.file_utils import add_start_docstrings, add_start_docstrings_to_callable
 from transformers.modeling_utils import PreTrainedModel, prune_linear_layer
