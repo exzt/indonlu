df58e66c8bb61a4c1492099e3c2d92714e75fe46
Huda Fathur <huda.fathurr@gmail.com>
2022-02-24 08:50:27 +0700

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -743,11 +743,11 @@ class QAFactoidDataLoader(DataLoader):
 #####
 class AspectBasedSentimentAnalysisAiryDataset(Dataset):
     # Static constant variable
-    ASPECT_DOMAIN = ['service', 'kebersihan','ruangan','makanan']
+    ASPECT_DOMAIN = ['service', 'kebersihan','ruangan','makanan','fasilitas']
     LABEL2INDEX = {'negative': 0, 'neutral': 1, 'positive': 2}
     INDEX2LABEL = {0: 'negative', 1: 'neutral', 2: 'positive'}
     NUM_LABELS = [3, 3, 3, 3]
-    NUM_ASPECTS = 4
+    NUM_ASPECTS = 5
     
     def load_dataset(self, path):
         df = pd.read_csv(path)
